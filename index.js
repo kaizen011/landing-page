04a1e5979783c9e5d065c72ec7722ca63223af89
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-07-22 08:00:00 -0500

    Added completed files.

---
+++ index.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Call of Duty: Black Ops 2</title>
+  <style>
+    body, h1, p {
+      margin: 0;
+      padding: 0;
+    }
+
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #222;
+      color: #fff;
+      line-height: 1.6;
+    }
+
+    /* Main section styles */
+    .main-section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-image: url('black_ops_2_background.jpg');
+      background-size: cover;
+      background-position: center;
+      color: #fff;
+      text-align: center;
+      padding: 20px;
+    }
+
+    .main-section h1 {
+      font-size: 64px;
+      margin-bottom: 20px;
+    }
+
+    .main-section p {
+      font-size: 24px;
+      max-width: 600px;
+      margin: 0 auto;
+    }
+
+    .cta-button {
+      display: inline-block;
+      padding: 10px 30px;
+      font-size: 20px;
+      background-color: #ff8000;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 5px;
+      margin-top: 20px;
+    }
+
+    .cta-button:hover {
+      background-color: #cc6600;
+    }
+
+    footer {
+      background-color: #111;
+      color: #fff;
+      text-align: center;
+      padding: 20px 0;
+    }
+  </style>
+</head>
+<body>
+  <div class="main-section">
+    <div class="content">
+      <h1>Call of Duty: Black Ops 2</h1>
+      <p>Experience the thrill of the ultimate first-person shooter in Call of Duty: Black Ops 2. Engage in intense combat, immerse yourself in a gripping storyline, and test your skills in various multiplayer modes.</p>
+      <a href="#" class="cta-button">Play Now</a>
+    </div>
+  </div>
+  <footer>
+    <p>&copy; 2023 Call of Duty: Black Ops 2. All rights reserved.</p>
+  </footer>
+</body>
+</html>
+
